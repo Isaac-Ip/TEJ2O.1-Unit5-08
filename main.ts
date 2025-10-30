a816295bae95b7a7e372c466a1ac88e542337d32
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-10-30 17:02:47 -0400

    forever statements

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 Isaac Ip All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Isaac Ip
+ * Created on: Oct 2025
+ * This program uses forever statements.
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+while (true) {
+    distanceToObject = sonar.ping(
+        DigitalPin.P0,
+        DigitalPin.P1,
+        PingUnit.Centimeters
+    )
+    basic.clearScreen()
+    basic.showNumber(distanceToObject)
+    if (distanceToObject < 10) {
+        basic.pause(500)
+        robotbit.StpCarMove(-10, 48)
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+    }
+    robotbit.StpCarMove(10, 48)
+}
